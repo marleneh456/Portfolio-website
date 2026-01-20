8ebe17e8a017a31f80aed8e81bf86a8a1262fb76
MarleneH24 <MarleneHabib56@gmail.com>
2026-01-20 08:28:23 -0600

    update

--- js/script.js
+++ js/script.js
@@ -255,7 +255,7 @@ function updateClock() {
   const minutes = now.getMinutes().toString().padStart(2, "0");
   const seconds = now.getSeconds().toString().padStart(2, "0");
 
-  const ampm = hours >= 12 ? "PM" : "AM";
+  const ampm = hours >= 12 ? "pm" : "am";
   hours = hours % 12 || 12;
 
   const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
